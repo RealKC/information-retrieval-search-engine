0232ae3a8e42c517e5834eff7b0c53c5a5666058
Dumitru Mițca <dumitru0mitca@gmail.com>
2025-04-03 13:25:33 +0300

    Lab6: Add a package for indexing-related code

---
+++ packages/indexing/src/indexing/direct.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from io import TextIOWrapper
+from typing import Iterable
+from bplustree import BPlusTree
+import stemmer
+from trie.trie import Trie
+
+from .utils import remove_special_characters, is_exception
+
+
+def build_direct_index(
+    files: Iterable[TextIOWrapper],
+    stopwords: Trie,
+    exceptions: Trie,
+) -> BPlusTree:
+    direct_index = BPlusTree()
+
+    for file in files:
+        words = Counter()
+        for line in file:
+            for word in line.split():
+                word = remove_special_characters(word)
+                if len(word) == 0:
+                    continue
+                elif is_exception(exceptions, word):
+                    words[word] += 1
+                elif stopwords.contains(word):
+                    continue
+                else:
+                    stem = stemmer.stem(word)
+                    words[stem] += 1
+        direct_index.insert(file, words)
+
+    return direct_index
---
+++ packages/indexing/src/indexing/utils.py
@@ -0,0 +1,33 @@
+from functools import reduce
+import re
+from trie.trie import Trie
+
+
+def remove_special_characters(word: str) -> str:
+    word = Trie.make_safe(word.lower())
+
+    return re.sub(r"[\#\$\[\]\(\)\{\}\*\.\,%&_=;:\-\?!]*", "", word)
+
+
+def is_exception(exceptions: Trie, word: str) -> bool:
+    if exceptions.contains(word):
+        return True
+
+    return reduce(lambda acc, ch: acc or ch.isdigit(), word, False)
+
+
+def parse_word_file(path: str) -> Trie:
+    trie = Trie()
+    with open(path, "r") as f:
+        for word in f:
+            word = word.strip()
+
+            if len(word) == 0 or word.startswith("#"):
+                continue
+
+            word = word.lower()
+            trie.insert(word)
+            if "'" in word:
+                trie.insert(re.sub("'", "", word))
+
+    return trie

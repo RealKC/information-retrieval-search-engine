9e9e02a8945aa409282a2931482f61fc96a772ea
Mițca Dumitru <dumitru0mitca@gmail.com>
2025-05-31 23:08:43 +0300

    Move labs to a `Labs/` folder

--- Labs/Lab3/run.sh
+++ Labs/Lab3/run.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 mkdir -p build
 
-g++ -Wall -Werror -Wno-sign-compare -std=c++23 -Og -g3 -I../packages/bplustree/include main.cpp -o build/main
+g++ -Wall -Werror -Wno-sign-compare -std=c++23 -Og -g3 -I../../packages/bplustree/include main.cpp -o build/main
 
 # gdb --args build/main $1 $2
 # valgrind --leak-check=full build/main $1 $2
